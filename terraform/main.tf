5caa1fb163b3f9b4dfa4dc870ad80fa07bba4a56
Soner Gunay <soner.gunay@agilisys.co.uk>
2022-05-16 13:47:25 +0100

    rrr

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "VPC"
+  }
+}
 resource "aws_instance" "web" {
   instance_type = var.instance_type
   ami           = var.aws_ec2_ami
